321810076a54f54c6ae106664bf77f6a373bfcc9
tugapi <kguoj@outlook.com>
2023-12-20 19:54:02 +0800

    Add glow_model.py

---
+++ glow_adversial/glow_model.py
@@ -0,0 +1,326 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from math import log, pi, exp
+import numpy as np
+from scipy import linalg as la
+
+logabs = lambda x: torch.log(torch.abs(x))
+
+
+class AdaIN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, content, style_std, style_mean):
+        assert (content.size()[:2] == style_std.size()[:2]) and (content.size()[:2] == style_mean.size()[:2])
+        size = content.size()
+        content_mean, content_std = self.calc_mean_std(content)
+        normalized_feat = (content - content_mean.expand(size)) / content_std.expand(size)
+        return normalized_feat * style_std.expand(size) + style_mean.expand(size)
+
+    def calc_mean_std(self, feat, eps=1e-5):
+        size = feat.size()
+        assert (len(size) == 4)
+        N, C = size[:2]
+        feat_var = feat.view(N, C, -1).var(dim=2) + eps
+        feat_std = feat_var.sqrt().view(N, C, 1, 1)
+        feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
+        return feat_mean, feat_std
+
+
+class ActNorm(nn.Module):
+    def __init__(self, in_channel):
+        super().__init__()
+
+        self.loc = nn.Parameter(torch.zeros(1, in_channel, 1, 1))
+        self.scale = nn.Parameter(torch.ones(1, in_channel, 1, 1))
+
+        self.register_buffer('initialized', torch.tensor(0, dtype=torch.uint8))
+
+    def initialize(self, input):
+        with torch.no_grad():
+            flatten = input.permute(1, 0, 2, 3).contiguous().view(input.shape[1], -1)
+            mean = (
+                flatten.mean(1)
+                    .unsqueeze(1)
+                    .unsqueeze(2)
+                    .unsqueeze(3)
+                    .permute(1, 0, 2, 3)
+            )
+            std = (
+                flatten.std(1)
+                    .unsqueeze(1)
+                    .unsqueeze(2)
+                    .unsqueeze(3)
+                    .permute(1, 0, 2, 3)
+            )
+            self.loc.data.copy_(-mean)
+            self.scale.data.copy_(1 / (std + 1e-6))
+
+    def forward(self, input):
+        _, _, height, width = input.shape
+
+        if self.initialized.item() == 0:
+            self.initialize(input)
+            self.initialized.fill_(1)
+
+        return self.scale * (input + self.loc)
+
+    def reverse(self, output):
+        return output / self.scale - self.loc
+
+
+class InvConv2d(nn.Module):
+    def __init__(self, in_channel, out_channel=None):
+        super().__init__()
+
+        if out_channel is None:
+            out_channel = in_channel
+        weight = torch.randn(in_channel, out_channel)
+        q, _ = torch.qr(weight)
+        weight = q.unsqueeze(2).unsqueeze(3)
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, input):
+        _, _, height, width = input.shape
+        out = F.conv2d(input, self.weight)
+        return out
+
+    def reverse(self, output):
+        return F.conv2d(
+            output, self.weight.squeeze().inverse().unsqueeze(2).unsqueeze(3)
+        )
+
+
+class InvConv2dLU(nn.Module):
+    def __init__(self, in_channel, out_channel=None):
+        super().__init__()
+
+        if out_channel is None:
+            out_channel = in_channel
+        weight = np.random.randn(in_channel, out_channel)
+        q, _ = la.qr(weight)
+        w_p, w_l, w_u = la.lu(q.astype(np.float32))
+        w_s = np.diag(w_u)
+        w_u = np.triu(w_u, 1)
+        u_mask = np.triu(np.ones_like(w_u), 1)
+        l_mask = u_mask.T
+
+        w_p = torch.from_numpy(w_p)
+        w_l = torch.from_numpy(w_l)
+        w_s = torch.from_numpy(w_s)
+        w_u = torch.from_numpy(w_u)
+
+        self.register_buffer('w_p', w_p)
+        self.register_buffer('u_mask', torch.from_numpy(u_mask))
+        self.register_buffer('l_mask', torch.from_numpy(l_mask))
+        self.register_buffer('s_sign', torch.sign(w_s))
+        self.register_buffer('l_eye', torch.eye(l_mask.shape[0]))
+        self.w_l = nn.Parameter(w_l)
+        self.w_s = nn.Parameter(logabs(w_s))
+        self.w_u = nn.Parameter(w_u)
+
+    def forward(self, input):
+        _, _, height, width = input.shape
+        weight = self.calc_weight()
+        out = F.conv2d(input, weight)
+        return out
+
+    def calc_weight(self):
+        weight = (
+            self.w_p
+            @ (self.w_l * self.l_mask + self.l_eye)
+            @ ((self.w_u * self.u_mask) + torch.diag(self.s_sign * torch.exp(self.w_s)))
+        )
+
+        return weight.unsqueeze(2).unsqueeze(3)
+
+    def reverse(self, output):
+        weight = self.calc_weight()
+
+        return F.conv2d(output, weight.squeeze().inverse().unsqueeze(2).unsqueeze(3))
+
+
+class ZeroConv2d(nn.Module):
+    def __init__(self, in_channel, out_channel, padding=1):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channel, out_channel, 3, padding=0)
+        self.conv.weight.data.zero_()
+        self.conv.bias.data.zero_()
+        self.scale = nn.Parameter(torch.zeros(1, out_channel, 1, 1))
+
+    def forward(self, input):
+        out = F.pad(input, [1, 1, 1, 1], value=1)
+        out = self.conv(out)
+        out = out * torch.exp(self.scale * 3)
+
+        return out
+
+
+class Coupling(nn.Module):
+    def __init__(self, in_channel, filter_size=512, affine=True, use_sigmoid=True):
+        super().__init__()
+
+        self.affine = affine  # to use affine or additive coupling layer
+        self.use_sigmoid = use_sigmoid  # if to use sigmoid in affine transformation to make training more stable
+
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channel // 2, filter_size, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(filter_size, filter_size, 1),
+            nn.ReLU(inplace=True),
+            ZeroConv2d(filter_size, in_channel if self.affine else in_channel // 2),
+        )  # there is no restriction placed on the choice of the net structure
+
+        self.net[0].weight.data.normal_(0, 0.05)
+        self.net[0].bias.data.zero_()
+
+        self.net[2].weight.data.normal_(0, 0.05)
+        self.net[2].bias.data.zero_()
+
+    def forward(self, input):
+        in_a, in_b = input.chunk(2, 1)
+
+        if self.affine:
+            log_s, t = self.net(in_a).chunk(2, 1)
+            if self.use_sigmoid:
+                s = torch.sigmoid(log_s + 2)
+                out_b = (in_b + t) * s
+            else:
+                s = torch.exp(log_s)
+                out_b = s * in_b + t
+
+        else:
+            net_out = self.net(in_a)
+            out_b = in_b + net_out
+
+        return torch.cat([in_a, out_b], 1)
+
+    def reverse(self, output):
+        out_a, out_b = output.chunk(2, 1)
+
+        if self.affine:
+            log_s, t = self.net(out_a).chunk(2, 1)
+            if self.use_sigmoid:
+                s = torch.sigmoid(log_s + 2)
+                in_b = out_b / s - t
+            else:
+                s = torch.exp(log_s)
+                in_b = (out_b - t) / s
+
+        else:
+            net_out = self.net(out_a)
+            in_b = out_b - net_out
+
+        return torch.cat([out_a, in_b], 1)
+
+
+class Flow(nn.Module):
+    def __init__(self, in_channel, use_coupling=True, affine=True, conv_lu=True, use_sigmoid=True):
+        super().__init__()
+
+        self.actnorm = ActNorm(in_channel)
+
+        if conv_lu:
+            self.invconv = InvConv2dLU(in_channel)
+        else:
+            self.invconv = InvConv2d(in_channel)
+
+        self.use_coupling = use_coupling
+        if self.use_coupling:
+            self.coupling = Coupling(in_channel, affine=affine, use_sigmoid=use_sigmoid)
+
+    def forward(self, input):
+        input = self.actnorm(input)
+        input = self.invconv(input)
+        if self.use_coupling:
+            input = self.coupling(input)
+        return input
+
+    def reverse(self, input):
+        if self.use_coupling:
+            input = self.coupling.reverse(input)
+        input = self.invconv.reverse(input)
+        input = self.actnorm.reverse(input)
+
+        return input
+
+
+class Block(nn.Module):
+    def __init__(self, in_channel, n_flow, split=True, affine=True, conv_lu=True, use_sigmoid=True):
+        super().__init__()
+
+        squeeze_dim = in_channel * 4
+
+        self.flows = nn.ModuleList()
+        for i in range(n_flow):
+            self.flows.append(Flow(squeeze_dim, affine=affine, conv_lu=conv_lu, use_sigmoid=use_sigmoid))
+
+        self.split = split
+
+    def forward(self, input):
+        b_size, n_channel, height, width = input.shape
+        # squeeze operation
+        squeezed = input.view(b_size, n_channel, height // 2, 2, width // 2, 2)
+        squeezed = squeezed.permute(0, 1, 3, 5, 2, 4)
+        out = squeezed.contiguous().view(b_size, n_channel * 4, height // 2, width // 2)
+
+        for flow in self.flows:
+            out = flow(out)
+
+        if self.split:
+            out, z_new = out.chunk(2, 1)  # separate z from the Block output
+
+        else:
+            z_new = out  # treat the whole Block output as z
+
+        return out, z_new
+
+    def reverse(self, z, output):
+        if self.split:
+            input = torch.cat([output, z], 1)
+        else:
+            input = z
+
+        for flow in self.flows[::-1]:
+            input = flow.reverse(input)
+
+        b_size, n_channel, height, width = input.shape
+
+        unsqueezed = input.view(b_size, n_channel // 4, 2, 2, height, width)
+        unsqueezed = unsqueezed.permute(0, 1, 4, 2, 5, 3)
+        unsqueezed = unsqueezed.contiguous().view(
+            b_size, n_channel // 4, height * 2, width * 2
+        )
+
+        return unsqueezed
+
+
+class Glow(nn.Module):
+    def __init__(self, in_channel, n_flow, n_block, affine=True, conv_lu=True, use_sigmoid=True):
+        super().__init__()
+
+        self.blocks = nn.ModuleList()
+        n_channel = in_channel
+        for i in range(n_block - 1):
+            self.blocks.append(Block(n_channel, n_flow, affine=affine, conv_lu=conv_lu, use_sigmoid=use_sigmoid))
+            n_channel *= 2
+        self.blocks.append(Block(n_channel, n_flow, split=False, affine=affine, conv_lu=conv_lu, use_sigmoid=use_sigmoid))
+
+    def forward(self, input):
+        out = input
+        z_outs = []
+
+        for block in self.blocks:
+            out, z_new = block(out)
+            z_outs.append(z_new)
+
+        return z_outs
+
+    def reverse(self, z_list, reconstruct=False):
+        input = self.blocks[-1].reverse(z_list[-1])
+        for i, block in enumerate(self.blocks[-2::-1]):
+            input = block.reverse(input, z_list[-(i + 2)], reconstruct=reconstruct)
+        return input
